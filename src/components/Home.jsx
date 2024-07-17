90dca5dc17c666adfc69f71948dd1d441433a9dd
sarankumarj <sarankumarjeyabalan@gmail.com>
2024-07-17 16:02:51 +0530

    chore: Update home page image in Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,10 +5,10 @@ function Home() {
   return (
     <div className="home">
       <div>
-        <img src="https://tneacounselling.in/wp-content/uploads/2022/08/Saveetha-Engineering-College-Kancheepuram.jpg" alt="bg image" />
-      </div>
       <h2>Welcome to Saveetha Engineering College</h2>
       <p>Inspiring future innovators and leaders.</p>
+        <img src="https://tneacounselling.in/wp-content/uploads/2022/08/Saveetha-Engineering-College-Kancheepuram.jpg" alt="bg image" />
+      </div>
     </div>
   );
 }
